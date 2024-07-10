c1b9721582c0a672e23077e79966af1644be788a
Dicko <macbook@Macbook-linux.local>
2024-07-10 16:16:55 +0700

    Add offline state

--- src/App.js
+++ src/App.js
@@ -6,25 +6,58 @@ import Arrived from './components/Arrived.js';
 import Clients from './components/Clients.js';
 import AsideMenu from './components/AsideMenu.js';
 import Footer from './components/Footer.js';
+import Offline from './components/Offline.js';
+
 
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [OfflineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  function handleOfflineStatus(){
+    setOfflineStatus(!navigator.onLine)
+  }
   
   React.useEffect(function() {
     (async function() {
-      const response = await fetch('https://bwacharity.fly.dev/items', {
-        headers:{
-          "Content-Type" : "application/json",
-          "accept" : "application/json"
+      console.log("render here")
+      try{
+        const response = await fetch('https://bwacharity.fly.dev/items', {
+            headers:{
+              "Content-Type" : "application/json",
+              "accept" : "application/json",
+            }
+          });
+          const { nodes } = await response.json();
+          setItems(nodes);
+    
+          if (!document.querySelector('script[src="/carousel.js"]')) {
+    
+          const script = document.createElement("script");
+          script.src = "/carousel.js";
+          script.async = false;
+          document.body.appendChild(script);
         }
-      });
-      const { nodes } = await response.json();
-      setItems(nodes);
+      } catch(e){
+        console.log("something error")
+      }
+
     })();
-  }, []);
+
+    handleOfflineStatus();
+    window.addEventListener('offline', handleOfflineStatus);
+    window.addEventListener('online', handleOfflineStatus);
+
+    return function(){
+      window.removeEventListener('offline', handleOfflineStatus);
+      window.removeEventListener('online', handleOfflineStatus);
+    }
+
+  }, [OfflineStatus]);
   return (
     <>
+
+      {OfflineStatus && <Offline />}
       <Header />,
       <Hero />,
       <Browse />,
---
+++ src/components/Offline.js
@@ -0,0 +1,18 @@
+function Offline(){
+return(
+    <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+        <div className="flex">
+            <div className="flex-shrink-0">
+
+            </div>
+            <div className="ml-3">
+                <p className="text-sm text-yellow-700">
+offline
+                </p>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default Offline;
